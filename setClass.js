cdea59f5e8c92f245c330fb9845bf0f76f3b4e1d
JRProgramming <ramirezjohnny@icloud.com>
2017-06-22 21:43:07 -0400

    Update setClass.js

--- setClass.js
+++ setClass.js
@@ -6,6 +6,9 @@ function logClasses()
     var algC1 = document.getElementById("AlgebraC")
     var ss = document.getElementById("SSG")
     var spa = document.getElementById("SpanishH")
+    if (algC1.checked ==  true){
+     alert("HEY")
+    }
 
  
 
