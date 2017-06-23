f9bfced3f7e42e25364bd4fd538e418f4a322007
JRProgramming <ramirezjohnny@icloud.com>
2017-06-22 22:34:39 -0400

    Update setClass.js

--- setClass.js
+++ setClass.js
@@ -1,13 +1,4 @@
-src="https://www.gstatic.com/firebasejs/4.1.3/firebase.js"
-var config = {
-    apiKey: "AIzaSyAPZwtE0Luy_umjawZcUXr5VbBeDlUvrlo",
-    authDomain: "bbclassmatch.firebaseapp.com",
-    databaseURL: "https://bbclassmatch.firebaseio.com",
-    projectId: "bbclassmatch",
-    storageBucket: "bbclassmatch.appspot.com",
-    messagingSenderId: "984402365769"
-  };
-firebase.initializeApp(config);
+
 function logClasses()
 {
  document.getElementById("heading").innerHTML = "Sup Johnny" 
