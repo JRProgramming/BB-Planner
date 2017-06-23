d9edb3aa2b13a7b071c9bdfe8dece1551ca0b4e0
JRProgramming <ramirezjohnny@icloud.com>
2017-06-22 22:49:59 -0400

    Update setClass.js

--- setClass.js
+++ setClass.js
@@ -3,14 +3,15 @@ function logClasses()
 {
  document.getElementById("heading").innerHTML = "Sup Johnny" 
  var playersRef = firebase.database().ref("Classes/");
+ var class = []
  var algC4 = document.getElementById("AlgebraC")
  if(algC4.checked == true)
  {
-  alert("selected")
+  class.push(algC4.value)
  }
  playersRef.push({
    Classes: { 
-      classes: "Joe"
+      classes: class
    }
 });
 }
