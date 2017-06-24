9f1eb3e48337ead85100d6d73e909b050c6ba8fc
JRProgramming <ramirezjohnny@icloud.com>
2017-06-24 09:30:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function askRemoveNote(index, div){
 }
 
 var j = 0;
-
+window.j = 0
 /* Add a note w/o saving it */
 function addNote(title, text) {
 var playersRef = firebase.database().ref("Classes/");
