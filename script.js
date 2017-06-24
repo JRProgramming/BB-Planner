98f3dc48dc445726545d955b3e009a8d8b403b90
JRProgramming <ramirezjohnny@icloud.com>
2017-06-24 10:13:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ playersRef.on("child_added", function(data, prevChildKey) {
    var newPlayer = data.val();
     alert(newPlayer.classes[j])
     title = title || newPlayer.classes[j]
+    window.j += 1;
     
 });
     text = text || "Lorem ispum dolor sit amet...";
@@ -63,7 +64,7 @@ playersRef.on("child_added", function(data, prevChildKey) {
      btn.css("color", "white");
      btn.appendTo(menu);
      btn.html('<i class="material-icons">&#xe872;</i>');
-    window.j += 1;
+    
     return {card: div, title: title, text: text};
 }
 
