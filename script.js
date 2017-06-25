077b4be81584f05cbde23455c07d096cb62c1c14
JRProgramming <ramirezjohnny@icloud.com>
2017-06-25 18:36:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function askRemoveNote(index, div){
         removeNote(index, div);
 }
 
-var j = -1
-window.j = -1
+var j = 0
+window.j = 0
 
 /* Add a note w/o saving it */
 function addNote(title, text) {
