71f6e1c5739a0d64e4fd351a7d32a48ef7110aee
JRProgramming <ramirezjohnny@icloud.com>
2017-06-26 11:25:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ playersRef.on("child_added", function(data, prevChildKey) {
     title_text.val(title);
     title_text.appendTo(title_e);
     window.j += 1
-     });
+
        text = text || "Lorem ispum dolor sit amet..."; 
     var sup_text = $("<div/>", {
         "class": "mdl-card__supporting-text",
@@ -65,6 +65,7 @@ playersRef.on("child_added", function(data, prevChildKey) {
      btn.css("color", "white");
      btn.appendTo(menu);
      btn.html('<i class="material-icons">&#xe872;</i>');
+         });
     
     return {card: div, title: title, text: text};
         
