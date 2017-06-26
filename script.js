4fb6b32506f8deb5fbc0ebf1a013c865c4f3d690
JRProgramming <ramirezjohnny@icloud.com>
2017-06-26 11:06:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,11 @@ var playersRef = firebase.database().ref("Classes/");
 playersRef.on("child_added", function(data, prevChildKey) {
    var newPlayer = data.val();
     title = title || newPlayer.classes[window.j]
+     var div = $("<div/>", {
+        "class": "mdl-card mdl-shadow--2dp note"            
+    });
+    
+    div.appendTo(".mdl-layout__content");  
 var title_e = $("<div/>", {
         "class": "mdl-card__title" 
     });
@@ -40,11 +45,7 @@ var title_e = $("<div/>", {
    }); 
     text = text || "Lorem ispum dolor sit amet...";
    
-    var div = $("<div/>", {
-        "class": "mdl-card mdl-shadow--2dp note"            
-    });
-    
-    div.appendTo(".mdl-layout__content");    
+     
     
        
     var sup_text = $("<div/>", {
