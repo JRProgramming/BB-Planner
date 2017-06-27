65f5331c33360724d396e4fcef25644c1eba84e8
JRProgramming <ramirezjohnny@icloud.com>
2017-06-27 08:24:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,8 @@ playersRef.on("child_added", function(data, prevChildKey) {
    });
     div.appendTo(".mdl-layout__content");
         
-    title = title || = window.johnny
+    alert(window.johnny)
+    
     
     var title_e = $("<div/>", {
         "class": "mdl-card__title" 
