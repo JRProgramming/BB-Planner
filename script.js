e1c4404ab9523364cf9f7567ab53e815428d0842
JRProgramming <ramirezjohnny@icloud.com>
2017-06-29 08:17:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ function addNote(title, text) {
 
 /* Add a note and save it (aka create a note) */
 function addAndSaveNote(title, text, index) {
-fo(i=0; i<window.lengths; i+=1){
+for(i=0; i<window.lengths; i+=1){
     var note = addNote(title, text);
     var obj = {
         title: window.johnny,
