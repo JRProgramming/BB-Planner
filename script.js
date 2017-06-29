14c4787dc75f4302dc5dd57a24552529d1acdf06
JRProgramming <ramirezjohnny@icloud.com>
2017-06-28 23:11:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function addNote(title, text) {
    });
     div.appendTo(".mdl-layout__content")
     title = title || window.johnny[window.j]
-    alert(window.johnny[window.j])
+    alert(window.j)
     
     var title_e = $("<div/>", {
         "class": "mdl-card__title" 
