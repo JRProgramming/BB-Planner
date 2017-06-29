25f5409a7e8c393a529171d43aacc79eae045c6e
JRProgramming <ramirezjohnny@icloud.com>
2017-06-29 13:08:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ function addNote(title, text) {
 }
 
 /* Add a note and save it (aka create a note) */
-function addAndSaveNote(title, text, index) {
+function addAndSaveNote() {
     
 for(i=0; i<window.lengths; i+=1){
 alert("HEY!");    
