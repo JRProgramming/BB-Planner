d2928943da595ac6278ecbb18babf0b9dda28842
JRProgramming <ramirezjohnny@icloud.com>
2017-07-01 08:56:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function addNote(title, text) {
         "class": "mdl-card mdl-shadow--2dp note"            
    });
     div.appendTo(".mdl-layout__content")
-    title = title || "";
+    title = title || johnny[j];
     
     var title_e = $("<div/>", {
         "class": "mdl-card__title" 
@@ -79,7 +79,7 @@ function addNote(title, text) {
 function addAndSaveNote(title, text, index) {
     var note = addNote(title, text);
     var obj = {
-        title: window.johnny[window.j],
+        title: note.title,
         text: note.text
     };
     if(typeof index === "number") {
