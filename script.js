20c18d377a0378b1a9a5255b8fe40d5e159d5f60
JRProgramming <ramirezjohnny@icloud.com>
2017-07-01 08:22:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ function addAndSaveNote(title, text, index) {
 }
 function loadNotes() {
     try {
-       var note = addNote(title, text);
+       var note = addAndSaveNote();
         if(note)
             note.forEach(function(item, index) {
                 if(item)
