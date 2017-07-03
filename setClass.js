bddde271de89944c36716c26dbf90bb7da9e95da
JRProgramming <ramirezjohnny@icloud.com>
2017-07-03 15:25:39 -0400

    Update setClass.js

--- setClass.js
+++ setClass.js
@@ -46,6 +46,7 @@ classes.push("Specials")
       classes: classes
 });
 document.getElementById("joe").innerHTML = "<button onclick=\"done()\">Go Back to the Planner</button>"
+ alert(window.email);
 } 
 function done(){  
  location.href = "index.html"
