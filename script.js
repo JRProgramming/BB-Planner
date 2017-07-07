c569a744ffe1a2c80b7662408992007308e870d5
JRProgramming <ramirezjohnny@icloud.com>
2017-07-06 23:29:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ playersRef.on("child_added", function(data, prevChildKey) {
   
    
   });
-var ref = firebase.database().ref("Classes/" + window.identification);
+var ref = firebase.database().ref("Classes/Completion");
 ref.once("value")
   .then(function(snapshot) {
     var a = snapshot.exists();  // true
