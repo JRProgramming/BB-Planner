75158c2bfeb1746d4dc34963b9d2f99e0dc196a0
JRProgramming <ramirezjohnny@icloud.com>
2017-07-15 23:05:06 -0400

    Update login.js

--- login.js
+++ login.js
@@ -149,6 +149,7 @@ localStorage.setItem("password", window.password);
  localStorage.setItem("email", "");
 localStorage.setItem("password", "");  
  }
+   document.getElementById("remember").checked = false;
    location.reload();
  }
   });
