5468c9af3b5bbd40020da8770fe567db80cd0899
JRProgramming <ramirezjohnny@icloud.com>
2017-07-15 22:35:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var playersRef = firebase.database().ref("Classes/");
 playersRef.on("child_added", function(data, prevChildKey) {
   var joey = data.val()
   var names = sessionStorage.getItem("name")
+  alert(names);
   if(names == null)
   {
   document.getElementById('id01').style.display='block';
