02b3a82ee37948fd86f7cb367a73e68f40a0b7cf
JRProgramming <ramirezjohnny@icloud.com>
2017-07-16 14:23:29 -0400

    Update login.js

--- login.js
+++ login.js
@@ -353,4 +353,8 @@ if(document.getElementById("email").value == "" && document.getElementById("pass
 {
 document.getElementById("remember").checked = false
 } 
+else if(document.getElementById("email").value != "" && document.getElementById("password").value != "" && document.getElementById("remember").checked == true)
+{
+setclass();
+}
 }
