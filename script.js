33cf8544926fbe5e8c007fd0bfff15e9dd7bda28
JRProgramming <ramirezjohnny@icloud.com>
2017-07-18 21:47:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ playersRef.on("child_added", function(data, prevChildKey) {
   if(window.kool = 1){
  window.johnny = joey.classes;
  window.lengths = joey.classes.length
+ alert(joey.homework);
    loadNotes() 
    }
   }
