2e358df3464dfb7fcd990de04c4e3cbd9f44a0e0
JRProgramming <ramirezjohnny@icloud.com>
2017-07-18 20:35:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ window.kool = 1
 function updateSave()
 {
 
- var classers = firebase.database().ref("Classes/" + window.identification + "/classes/" + window.numj + "/" + window.hat);
+ var classers = firebase.database().ref("Classes/" + window.identification + "/homework/" + window.hat);
  classers.update({
      homework: window.texter
 });
