d7300227f8bb92e64c549678f8c8cf56ec384858
JRProgramming <ramirezjohnny@icloud.com>
2017-07-18 20:24:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -113,14 +113,13 @@ function addAndSaveNote(title, text, index) {
       }
        if(window.classes[window.numj] == note.title)
        {
+         alert(window.numj)
          window.classj = window.numj;
          window.hat = note.title;
          window.texter = $(this).html();
          window.numj = 0;
                updateSave();
        }
-
-          alert(window.numj);
        
      }
     note.card.find(".mdl-card__supporting-text").on("input", onUp); 
