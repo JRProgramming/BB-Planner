8f2085249185be9082e9fc473e8c0c84a9edb04f
JRProgramming <ramirezjohnny@icloud.com>
2017-07-21 06:12:07 -0400

    Update otherPlanner.js

--- otherPlanner.js
+++ otherPlanner.js
@@ -62,8 +62,7 @@ function addAndSaveNote(title, index) {
 }
 
 function loadNotes() {
-  alert(window.names.length)
-       for(i=0;i<window.names.length;i++){
+       for(i=0;i<window.number;i++){
        var note = addAndSaveNote();
         if(note)
      function load(item, index) {
