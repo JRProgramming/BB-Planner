4c170350a27d372c1022a63347ef3585c0d9da57
JRProgramming <ramirezjohnny@icloud.com>
2017-07-22 17:24:19 -0400

    Update otherPlanner.js

--- otherPlanner.js
+++ otherPlanner.js
@@ -34,7 +34,7 @@ function addNote(title, text) {
    
      text = text || "Select"; 
     var sup_text = $("<a/>", {
-        "class": "mdl-card__supporting-text",
+        "class": "mdl-button mdl-js-button"",
         "href": "index.html",
         "contenteditable": "false"
     });
