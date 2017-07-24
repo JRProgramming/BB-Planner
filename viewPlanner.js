65bbca9973ada97408bb280c09a4683744b863fb
JRProgramming <ramirezjohnny@icloud.com>
2017-07-24 19:18:23 -0400

    Update viewPlanner.js

--- viewPlanner.js
+++ viewPlanner.js
@@ -1,6 +1,7 @@
 window.numb = 0
 window.kh = 0
 window.array = [];
+window.j = 0
 var playersRef = firebase.database().ref("Classes/");
 playersRef.on("child_added", function(data, prevChildKey) {
   var joey = data.val()
@@ -35,3 +36,71 @@ window.numb += 1
   }
 }
 }
+function addNote(title, text) {
+   var div = $("<div/>", {
+        "class": "mdl-card mdl-shadow--2dp note"            
+   });
+    div.appendTo(".mdl-layout__content")
+        
+  title = title || window.array[window.j];
+        
+    var title_e = $("<div/>", {
+        "class": "mdl-card__title", 
+        "contenteditable": "false"
+    });
+    title_e.appendTo(div);
+        
+    var title_text = $("<input>", {
+        "class": "mdl-card__title-text"
+    });
+    
+    title_text.val(title);
+    title_text.appendTo(title_e);
+   
+
+       text = text || ""; 
+    var sup_text = $("<div/>", {
+        "class": "mdl-card__supporting-text",
+        "contenteditable": ""
+    });
+    sup_text.html(text);
+    sup_text.appendTo(div);
+    
+    var menu = $("<div/>", {
+        "class": "mdl-card__menu" 
+    });
+    menu.appendTo(div);
+    
+    window.j += 1
+     
+    return {card: div, title: title, text: text};
+  
+}
+
+window.classes = [];
+window.numj = 0
+function addAndSaveNote(title, text, index) {
+ 
+    var note = addNote(title, text);
+    window.obj = {
+        title: note.title,
+        text: note.text
+    }
+
+     
+
+    return note;
+    
+}
+
+function loadNotes() {
+       for(i=0;i<window.fclass.length;i++){
+       var note = addAndSaveNote();
+        if(note)
+     function load(item, index) {
+                if(item)
+                    addAndSaveNote(item.title, item.text, index); 
+            };
+       }
+}
+
