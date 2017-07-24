6f248c9657a4467923c2406100e197037e73af66
JRProgramming <ramirezjohnny@icloud.com>
2017-07-24 08:31:38 -0400

    Update viewPlanner.js

--- viewPlanner.js
+++ viewPlanner.js
@@ -2,7 +2,9 @@ var playersRef = firebase.database().ref("Classes/");
 playersRef.on("child_added", function(data, prevChildKey) {
   var joey = data.val()
   var names = sessionStorage.getItem("name")
+  alert(names)
  var nameS = sessionStorage.getItem("nameS");
+  alert(nameS);
    if(joey.name == names)
    {
 alert(joey.classes);
