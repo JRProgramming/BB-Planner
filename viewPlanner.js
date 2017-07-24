765924c2bcc55a9d9de88cf15b74969b65abeab2
JRProgramming <ramirezjohnny@icloud.com>
2017-07-24 19:56:12 -0400

    Update viewPlanner.js

--- viewPlanner.js
+++ viewPlanner.js
@@ -28,12 +28,7 @@ for(i=0;i<window.array;i++)
 {
   console.log(window.identification);
   console.log(window.fclass);
-var playersRef = firebase.database().ref("Classes/" + window.identification + "/Homework/"+ window.fclass[i]);
-playersRef.on("child_added", function(data, prevChildKey) {
-  var joey =  data.val()
-  window.hw.push(joey);
 
-})
 }
 }
 function match()
