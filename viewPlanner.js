bf6176b7969bbca99b992fbf71e1f646c7d5a5a0
JRProgramming <ramirezjohnny@icloud.com>
2017-07-24 08:25:09 -0400

    Update viewPlanner.js

--- viewPlanner.js
+++ viewPlanner.js
@@ -1,1 +1,67 @@
+function addNote(title, text) {
+   var div = $("<div/>", {
+        "class": "mdl-card mdl-shadow--2dp note"            
+   });
+    div.appendTo(".mdl-layout__content")
+        
+  title = title || window.johnny[window.j];
+        
+    var title_e = $("<div/>", {
+        "class": "mdl-card__title" 
+    });
+    title_e.appendTo(div);
+        
+    var title_text = $("<input>", {
+        "class": "mdl-card__title-text"
+    });
+    
+    title_text.val(title);
+    title_text.appendTo(title_e);
+   
 
+       text = text || window.hw[window.j]; 
+    var sup_text = $("<div/>", {
+        "class": "mdl-card__supporting-text",
+        "contenteditable": ""
+    });
+    sup_text.html(text);
+    sup_text.appendTo(div);
+    
+    var menu = $("<div/>", {
+        "class": "mdl-card__menu" 
+    });
+    menu.appendTo(div);
+    
+    window.j += 1
+     
+    return {card: div, title: title, text: text};
+  
+}
+
+window.classes = [];
+window.numj = 0
+function addAndSaveNote(title, text, index) {
+ 
+    var note = addNote(title, text);
+    window.obj = {
+        title: note.title,
+        text: note.text
+    }
+    window.classes.push(note.title)
+    
+     note.onUp = onUp;
+ 
+    return note;
+    
+}
+
+function loadNotes() {
+       for(i=0;i<window.lengths;i++){
+       var note = addAndSaveNote();
+        if(note)
+     function load(item, index) {
+                if(item)
+                    addAndSaveNote(item.title, item.text, index); 
+            };
+       }
+}
