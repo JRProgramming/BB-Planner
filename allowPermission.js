cf5a2321f2e7ffb954db96f18b059c476efeb87c
JRProgramming <ramirezjohnny@icloud.com>
2017-07-25 18:23:24 -0400

    Update allowPermission.js

--- allowPermission.js
+++ allowPermission.js
@@ -14,8 +14,8 @@ window.names = [];
   loadNotes()
   if(window.yuko == 1)
   {
-    console.log(window.name[window.gf])
-  if(window.name[window.gf] == joey.name)
+    console.log(window.nameh[window.gf])
+  if(window.nameh[window.gf] == joey.name)
   {
      window.identification = joey.ID
     console.log(window.identification)
