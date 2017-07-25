2e4d1269c7721e421073e25541740dfbd52ecee7
JRProgramming <ramirezjohnny@icloud.com>
2017-07-25 19:11:15 -0400

    Update allowPermission.js

--- allowPermission.js
+++ allowPermission.js
@@ -71,9 +71,9 @@ function addAndSaveNote(title, index) {
      $("input[type=\"checkbox\"]").on("click", function() {
       if(window.b == 0)
       {
+        console.log(window.nameh
        window.nameh.push(note.title);
-        console.log(window.nameh);
-        
+        console.log(note.title);
       }
     });
     
