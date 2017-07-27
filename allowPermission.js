55468e33806d782c14ba025dbb10e43aecc368d7
JRProgramming <ramirezjohnny@icloud.com>
2017-07-26 20:26:12 -0400

    Update allowPermission.js

--- allowPermission.js
+++ allowPermission.js
@@ -9,10 +9,10 @@ window.b = 0;
 var playersRef = firebase.database().ref("Classes/");
 playersRef.on("child_added", function(data, prevChildKey) {
   var joey = data.val()
-  window.names = [];
   if(sessionStorage.getItem("name") != joey.name)
   {
   window.names.push(joey.name);
+    console.log(window.names);
   }
   loadNotes()
 
