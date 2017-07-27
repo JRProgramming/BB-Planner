880ea23918abe916b950bc99fa8f8edfdd056dea
JRProgramming <ramirezjohnny@icloud.com>
2017-07-26 20:53:51 -0400

    Update allowPermission.js

--- allowPermission.js
+++ allowPermission.js
@@ -1,14 +1,21 @@
-window.names = []
+window.names = [];
+window.joe = [];
+window.nameh = []
+window.hat = "";
 window.number = 0;
+window.numj = 0;
+window.gf = 0;
+window.b = 0;
 var playersRef = firebase.database().ref("Classes/");
 playersRef.on("child_added", function(data, prevChildKey) {
   var joey = data.val()
-window.names = [];
   if(sessionStorage.getItem("name") != joey.name)
   {
   window.names.push(joey.name);
+    console.log(window.names);
   }
   loadNotes()
+
   });
 
 
@@ -51,13 +58,14 @@ function addNote(title, text) {
     menu.appendTo(div);
     
     window.number += 1
+  
      
     return {card: div, title: title};
   
 }
 
 
-window.b = 0
+
 function addAndSaveNote(title, index) {
  
     var note = addNote(title);
@@ -65,11 +73,8 @@ function addAndSaveNote(title, index) {
         title: note.title,
     }
 
-     $("input[type=\"checkbox\"]").on("click", function() {
-      if(window.b == 0)
-      {
-       alert(note.title);
-      }
+     note.card.find("input[type=\"checkbox\"]").on("click", function() {
+window.nameh.push(note.title);
     });
     
    
@@ -77,9 +82,30 @@ function addAndSaveNote(title, index) {
     return note;
     
 }
+function loadData()
+{
+  
+
+    var playersRef = firebase.database().ref("Classes/");
+playersRef.on("child_added", function(data, prevChildKey) {
+  var joey = data.val()
+  if(sessionStorage.getItem("name") == joey.name)
+  {
+     window.identification = joey.ID
+  }
+  
+    window.gf += 1
+})
+   var classers = firebase.database().ref("Classes/" + window.identification + "/Access")
+   classers.update({
+         access: window.nameh
+   })
+}
 
 function loadNotes() {
+  console.log(window.names.length);
        for(i=0;i<window.names.length;i++){
+         console.log("HEY");
        var note = addAndSaveNote();
         if(note)
      function load(item, index) {
