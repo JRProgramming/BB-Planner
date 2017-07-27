27bde04fcaf030fe4a855936eb63a4365a72656f
JRProgramming <ramirezjohnny@icloud.com>
2017-07-27 16:40:52 -0400

    Update requestPermission.js

--- requestPermission.js
+++ requestPermission.js
@@ -116,10 +116,6 @@ if(joey.name != sessionStorage.getItem("name"))
   {
      window.identification = joey.ID;
     console.log(window.identification)
-    var classers = firebase.database().ref("Classes/" + window.identification)
-   classers.update({
-         Access: window.nameh
-   })
   }
   
     window.gf += 1
