3b86c439694aa0ec99cf370ced631c311d90d156
JRProgramming <ramirezjohnny@icloud.com>
2017-07-27 16:51:30 -0400

    Update requestPermission.js

--- requestPermission.js
+++ requestPermission.js
@@ -111,14 +111,13 @@ if(joey.name != sessionStorage.getItem("name"))
    {
  window.nameb.push(joey.name)
 }
- console.log(window.nameh[window.gf]);
-  console.log(window.nameb[window.y]);
   if(window.nameh[window.gf] != undefined && window.nameb[window.y] != undefined)
   {
   if(window.nameh[window.gf] == window.nameb[window.y])
   {
      window.identification = joey.ID;
     console.log(window.identification)
+    window.y += 1
   }
 window.gf += 1
   }
