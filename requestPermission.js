7ebc13edb088c84ba661dbdc0b6692e913785cfa
JRProgramming <ramirezjohnny@icloud.com>
2017-07-28 21:49:43 -0400

    Update requestPermission.js

--- requestPermission.js
+++ requestPermission.js
@@ -115,7 +115,7 @@ if(joey.name != sessionStorage.getItem("name"))
    {
  window.nameb.push(joey.name)
 }
-    console.log(
+    console.log(window.nameh[window.y]);
   
   if(window.nameb[window.gf].includes(window.nameh[window.y]) == true)
   {
