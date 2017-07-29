e3c1e94e03a7ee4dcbf55120fb5dcd42cb5a8f06
JRProgramming <ramirezjohnny@icloud.com>
2017-07-28 23:43:22 -0400

    Update requestPermission.js

--- requestPermission.js
+++ requestPermission.js
@@ -150,10 +150,10 @@ window.ds.push(sessionStorage.getItem("name"))
         }
       }
     window.y += 1;
-             console.log(window.ds);
-  window.hy.push(window.ds) 
+      
     }
-
+       console.log(window.ds);
+  window.hy.push(window.ds) 
   }
 
 
