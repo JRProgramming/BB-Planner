79a15e0ef4193e15be19df5f4441b2a57f88e113
JRProgramming <ramirezjohnny@icloud.com>
2017-07-29 23:39:30 -0400

    Update acceptRequest.js

--- acceptRequest.js
+++ acceptRequest.js
@@ -1,4 +1,4 @@
-
+window.names = []
 window.tr = [];
 window.req = [];
 window.number = 0;
@@ -8,16 +8,17 @@ function load()
 var playersRef = firebase.database().ref("Classes/");
 playersRef.on("child_added", function(data, prevChildKey) {
   var joey = data.val()
+  window.names = [];
   if(sessionStorage.getItem("name") == joey.name)
   {
     if(joey.Request != undefined)
     {
- window.names = joey.Request
+window.names.push(joey.Request)
+    }
   }
-  }
-  loadNotes()  
-  });
 
+  loadNotes()
+  });
 }
 
 
@@ -83,7 +84,81 @@ function addAndSaveNote(title, index) {
     window.obj = {
         title: note.title,
     }
+   var playersRef = firebase.database().ref("Classes/");
+playersRef.on("child_added", function(data, prevChildKey) {
+  var joey = data.val()
+  if(sessionStorage.getItem("name") == joey.name)
+  {
+     window.identification = joey.ID
+  }
 
+});
+      note.card.find("a").on("click", function() {
+
+   var playersRef = firebase.database().ref("Classes/");
+playersRef.on("child_added", function(data, prevChildKey) {
+  var joey = data.val()
+if(joey.Access != undefined)
+{
+  window.tr = joey.Access
+}
+    
+    window.req = []
+  window.req.push(joey.Request)
+  console.log(joey.Request);
+  if(window.req[window.num] == note.title)
+  {
+window.req.splice(window.num, 1)
+  }
+})
+   
+for(i=0;i<window.tr.length;i++)
+{
+if(window.tr[i] != note.title)
+{
+window.gf = "Nothing";
+}
+  else
+  {
+   window.gf = "Something"; 
+  }
+
+}
+        if(window.tr.length == 0)
+        {
+window.gf = "Nothing"
+        }
+        
+if(window.gf == "Nothing")
+{
+window.tr = note.title[0]
+}
+        console.log(window.tr)
+        console.log(window.req)
+   var classers = firebase.database().ref("Classes/" + window.identification)
+classers.update({
+       Access: window.tr,
+       Request: window.req
+});
+});  
+      note.card.find("button").on("click", function() {
+           var playersRef = firebase.database().ref("Classes/");
+playersRef.on("child_added", function(data, prevChildKey) {
+  var joey = data.val()
+    window.req = []
+  window.req.push(joey.Request)
+  if(window.req[window.num] == note.title)
+  {
+window.req.splice(window.num, 1)
+  }
+  console.log(window.req)
+
+})
+           var classers = firebase.database().ref("Classes/" + window.identification)
+classers.update({
+       Request: window.req
+});
+      })
     
    
  
@@ -96,9 +171,6 @@ load()
 }
 
 function loadNotes() {
-  if(window.names.length != undefined)
-  {
-    console.log(window.names.length);
        for(i=0;i<window.names.length;i++){
        var note = addAndSaveNote();
         if(note)
@@ -107,6 +179,4 @@ function loadNotes() {
                     addAndSaveNote(item.title, index); 
             };
        }
-  }
 }
-
