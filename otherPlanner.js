8099b0775d20754bf1480378aca3fec6ba317a1d
JRProgramming <ramirezjohnny@icloud.com>
2017-08-01 07:54:24 -0400

    Update otherPlanner.js

--- otherPlanner.js
+++ otherPlanner.js
@@ -1,8 +1,8 @@
-window.names = []
 window.number = 0;
 var playersRef = firebase.database().ref("Classes/");
 playersRef.on("child_added", function(data, prevChildKey) {
   var joey = data.val()
+  window.names = []
   if(sessionStorage.getItem("name") == joey.name)
   {
     if(joey.Access != undefined)
