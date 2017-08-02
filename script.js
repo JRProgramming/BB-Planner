7ee09fdc6d095c0ddefa198ebd09ee8214b2f160
JRProgramming <ramirezjohnny@icloud.com>
2017-08-02 16:29:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -160,6 +160,7 @@ function addAndSaveNote(title, text, index) {
        if(window.classes[window.numj] == note.title)
        {
          window.hat = note.title;
+         $(this).html(window.texter + "<div>This is confirmation</div><br>")
          window.texter = $(this).html()
          updateSave();
           window.numj = 0;
