627108d05bce6f0268caf1f5d5d29e9c84b7df62
JRProgramming <ramirezjohnny@icloud.com>
2017-08-02 08:11:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -153,7 +153,7 @@ function addAndSaveNote(title, text, index) {
     note.card.find(".mdl-card__supporting-text").on("input", onUp); 
   note.card.find(".mdl-card__supporting-text").on("keydown", function (e) {
     if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
-        console.log($(this.html()));
+        console.log($(this).html());
     }
 });
      note.onUp = onUp;
