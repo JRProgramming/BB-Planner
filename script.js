9b7eb8a30d479afa263863f1d2f8edf863c3fb9c
JRProgramming <ramirezjohnny@icloud.com>
2017-08-03 08:50:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -161,7 +161,7 @@ function addAndSaveNote(title, text, index) {
        if(window.classes[window.numj] == note.title)
        {
          window.hat = note.title;
-         $(this).html(window.texter + "<div><input type=\"checkbox\"> </div>")
+         $(this).html(window.texter + "<br><input type=\"checkbox\"> </br>")
          window.texter = $(this).html()
             placeCaretAtEnd(this);
          updateSave();
