a40463c11328e4dcc2af6297f0048e17df80e358
JRProgramming <ramirezjohnny@icloud.com>
2017-08-04 17:08:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -162,7 +162,7 @@ function addAndSaveNote(title, text, index) {
        {
          window.hat = note.title
         console.log(window.identification)
-         var playersRef = firebase.database().ref("Classes/" + window.identification + "/Homework/"+ window.hat);
+         var playersRef = firebase.database().ref("Classes/" + window.id + "/Homework/"+ window.hat);
 playersRef.on("child_added", function(data, prevChildKey) {
  var joey = data.val()
  alert(joey)
