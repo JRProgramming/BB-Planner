6a668ae0440b49a5ffc329efca471bb6047e18b1
JRProgramming <ramirezjohnny@icloud.com>
2017-08-04 09:36:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -154,6 +154,7 @@ function addAndSaveNote(title, text, index) {
        if(window.classes[window.numj] == note.title)
        {
          window.hat = note.title;
+         window.texter.push(note.card.find(".mdl-card__supporting-text").html())
          window.texter.push("<input type=\"checkbox\">" + $(this).html() + "<br>")
          updateSave();
          note.card.find(".mdl-card__supporting-text").html(window.texter)
