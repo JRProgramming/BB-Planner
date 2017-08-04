ff62892bdeb8553c5a5a5e5bdb7205c8e2a1d200
JRProgramming <ramirezjohnny@icloud.com>
2017-08-04 09:45:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -158,6 +158,7 @@ function addAndSaveNote(title, text, index) {
          window.hat = note.title;
          window.texter.push(note.card.find(".mdl-card__supporting-text").html())
          window.texter.push("<input type=\"checkbox\">" + $(this).html() + "<br>")
+         console.log(note.card.find(".mdl-card__supporting-text").html())
          note.card.find(".mdl-card__supporting-text").html("");
          updateSave();
          note.card.find(".mdl-card__supporting-text").html(window.texter)
