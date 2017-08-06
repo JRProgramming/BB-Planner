d5cbb016b8b50b6b4be1b27acef2cd8560c31136
JRProgramming <ramirezjohnny@icloud.com>
2017-08-05 20:04:35 -0400

    Update otherPlanner.js

--- otherPlanner.js
+++ otherPlanner.js
@@ -102,7 +102,7 @@ location.href = "index.html";
 }
 
 function loadNotes() {
-  alert(window.names);
+  alert(window.names.length);
        for(i=0;i<window.names.length;i++){
        var note = addAndSaveNote();
         if(note)
