3746c08cdbeee22d8bd045659cb6a7a898dcdf62
JRProgramming <ramirezjohnny@icloud.com>
2017-08-07 22:32:41 -0400

    Update allowPermission.js

--- allowPermission.js
+++ allowPermission.js
@@ -17,19 +17,20 @@ playersRef.on("child_added", function(data, prevChildKey) {
   }
 else
 {
-
+  
 var access = joey.Access
 
 }
-alert(access)
+
   if(access != undefined)
   {
-    
+   alert(access) 
 if(window.names.length != 0)
 {
+  alert(access)
   for(i=0;i<window.names.length; i++)
   {
-
+alert(access)
 if(access.indexOf(window.names[i]) != -1)
 {
 window.names.splice(i, 1)
