b3ae6c9d95fc34f18311e9a1b943b7099f3969f1
JRProgramming <ramirezjohnny@icloud.com>
2017-08-07 21:47:25 -0400

    Update allowPermission.js

--- allowPermission.js
+++ allowPermission.js
@@ -21,19 +21,7 @@ access = joey.Access
 
 }
   alert(access)
-   if(access.length != 0)
-  {
-if(window.names.length != 0)
-{
-  for(i=0;i<window.names.length)
-  {
-if(access.indexOf(window.names[i]) != -1)
-{
-window.names.splice(i, 1)
-}
-  }
-}
-  }
+
 
   loadNotes()
 
