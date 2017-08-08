ecdcbec83dc47f12ff6c2ece93d0cd21c56de966
JRProgramming <ramirezjohnny@icloud.com>
2017-08-08 14:46:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -182,8 +182,7 @@ joey = []
 })
     window.texter.push("<input type=\"checkbox\">" + $(this).html() + "<br>")
          updateSave();  
-        $(this).html("")
-        alert($(this).html())
+        $(this).html()
          window.numj = 0;
           
 }
