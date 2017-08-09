11ada04a0bfaf344376e78fa9bdbf6caaa7021c6
JRProgramming <ramirezjohnny@icloud.com>
2017-08-09 00:10:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ playersRef.on("child_added", function(data, prevChildKey) {
 window.names = joey.Request
   if(window.names.length != 0)
   {
-document.getElementById("accept").innerHTML = "See Other planners (" + window.names.length + ")"
+document.getElementById("access").innerHTML = "See other planners (" + window.names.length + ")"
   }
     
     }
