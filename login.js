ff5f6f353ced04ede17c25a0eca2fd297f34bade
JRProgramming <ramirezjohnny@icloud.com>
2017-08-10 11:22:56 -0400

    Update login.js

--- login.js
+++ login.js
@@ -342,10 +342,9 @@ sendEmailVerification()
      * Sends an email verification to the user.
      */
     function sendEmailVerification() {
-      // [START sendemailverification]
+      alert(firebase.auth().currentUser)
       firebase.auth().currentUser.sendEmailVerification().then(function() {
         // Email Verification sent!
-        // [START_EXCLUDE]
         alert('Email Verification Sent!');
         // [END_EXCLUDE]
       });
