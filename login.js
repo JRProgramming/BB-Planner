ee7e600d273b66ebea2c022e2248cd00d6bc121a
JRProgramming <ramirezjohnny@icloud.com>
2017-08-10 17:28:14 -0400

    Update login.js

--- login.js
+++ login.js
@@ -281,6 +281,7 @@ else
         });
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
+     initApp()
     unique();
   } 
 });
@@ -333,6 +334,32 @@ return;
       toggleSignIn()
   } 
 });
+    }
+
+function initApp() {
+      // Listening for auth state changes.
+      // [START authstatelistener]
+      firebase.auth().onAuthStateChanged(function(user) {
+        // [START_EXCLUDE silent]
+         alert(user)
+        // [END_EXCLUDE]
+        if (user) {
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+           alert(email)
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          // [END_EXCLUDE]
+        } 
+        // [START_EXCLUDE silent]
+        
+        // [END_EXCLUDE]
+      });
+      
     }
 function logOut()
 {
