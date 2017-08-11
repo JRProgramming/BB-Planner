9d595686602268571408600b09c3395a06d998a4
JRProgramming <ramirezjohnny@icloud.com>
2017-08-11 00:15:19 -0400

    Update login.js

--- login.js
+++ login.js
@@ -347,6 +347,23 @@ location.href = "https://jrprogramming.github.io/BB-Planner";
      * Sends an email verification to the user.
      */
     function sendEmailVerification() {
+       firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+     alert(email)
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
       firebase.auth().currentUser.sendEmailVerification().then(function() {
         // Email Verification sent!
         alert('Email Verification Sent!');
