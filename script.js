114419287c4b0081a9c4ce382c010c0b64711650
JRProgramming <ramirezjohnny@icloud.com>
2017-08-12 10:55:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -205,7 +205,7 @@ joey = []
 })
         var alength = "x" + window.texter.length + 1;  
         alert(alength);
-    window.texter.push("<input type=\"checkbox\">" + $(this).val() + "<button id=" + alength + ">X</button>" + "<br>")
+    window.texter.push("<input type=\"checkbox\">" + $(this).val() + "<button>X</button>" + "<br>")
          updateSave();  
         $(this).val("")
          window.numj = 0;
