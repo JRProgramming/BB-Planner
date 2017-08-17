f9be331d7abbf75ebc8ddc6b9b6a2b9ce3ecc949
JRProgramming <ramirezjohnny@icloud.com>
2017-08-17 08:54:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -203,7 +203,7 @@ joey = []
         }
         if(tyr == window.lengthj)
         {
-        note.card.find(".mdl-card__title-text").html(note.title + "  <span style=\"text-decoration: underline; cursor:pointer; float: right;\" class=\"clearbutton\" id=\"" + classes + "buttonx\" onclick=\"clearHW()\">Clear HW</span>")
+        note.card.find(".mdl-card__title-text").html(note.title + "  <span style=\"text-decoration: underline; cursor:pointer; float:right;\" class=\"clearbutton\" id=\"" + classes + "buttonx\" onclick=\"clearHW()\">Clear HW</span>")
         }
         else
         {
