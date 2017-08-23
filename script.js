e6a6a8e121cea84f73bfcebdf473f82159b0afad
JRProgramming <ramirezjohnny@icloud.com>
2017-08-23 13:36:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -186,42 +186,6 @@ note.card.find(".mdl-card__menu").html("")
  window.texter = "No homework"
  updateSave()
 }
- 
-       var playersRef = firebase.database().ref("Classes/" + window.id + "/Homework/"+ note.title);
-playersRef.on("child_added", function(data, prevChildKey) {
- var joey = data.val()
- if(joey == "No homework")
- {
-joey = []
- }
-var tyrs = 0;
- var classes = note.title
-         if(classes == "Social Studies")
-        {
-        classes = "Social_Studies"
-        }
-for(i=0;i<joey.length;i++)
-{
-        if(document.getElementById(classes + i).checked == true)
-        {
-        tyrs ++
-        }
-}
- if(tyrs == joey.length && tyrs != 0)
-        {
-         if(note.card.find(".mdl-card__menu").html() == "")
-         {
-         var spaner = $("<span>", {id: classes + "buttonx", style: "text-decoration: underline; cursor:pointer; color: white;", html: "Clear HW"})
-         spaner.click(function() { clearHW() })
-        note.card.find(".mdl-card__menu").append(spaner)
-         }
-        }
-        else
-        {
-          $("#" + classes + "buttonx").remove()
-        }
-
-})
    note.card.find(".mdl-card__supporting-text").on("click", function(){
    window.hat = note.title;
   window.texter = [];
