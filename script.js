9f8278ece798a7ba9329f81982f94b3448dbcdbc
JRProgramming <ramirezjohnny@icloud.com>
2017-08-25 09:27:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -88,11 +88,11 @@ playersRef.on("child_added", function(data, prevChildKey) {
 }
 }
 function count(){
-alert(window.identification)
  var ref = firebase.database().ref("Classes/" + window.identification + "/name");
 ref.once("value")
   .then(function(snapshot) { 
   window.a = snapshot.exists();
+ alert(window.a)
  if(window.a == false)
  {
   location.href = "setClass" 
