59a8c0a995e9e8f8e6ddaca9733bb4fa03e285ec
JRProgramming <ramirezjohnny@icloud.com>
2017-08-27 14:09:11 -0400

    Update emailVerification.js

--- emailVerification.js
+++ emailVerification.js
@@ -5,3 +5,7 @@ document.getElementById("h1").innerHTML = "Welcome " + name
 document.getElementById("h3").innerHTML = "Your email hasn't been verified, please verify your email address." + "<br>"
 document.getElementById("h3").innerHTML = "When you verify your email, click the button below"
 }
+function home()
+{
+ location.href="https://jrprogramming.github.io/BB-Planner/"
+}
