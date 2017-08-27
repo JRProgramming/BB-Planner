ec2e8db5c2d31fa32e00a2a5aeb85140802be8b1
JRProgramming <ramirezjohnny@icloud.com>
2017-08-27 13:53:01 -0400

    Update emailVerification.js

--- emailVerification.js
+++ emailVerification.js
@@ -1,4 +1,7 @@
 window.onload = function()
 {
 var name = sessionStorage.getItem("v")
+document.getElementById("h1").innerHTML = "Welcome " + name
+document.getElementById("h3").innerHTML = "Your email hasn't been verified, please verify your email address." + "<br>"
+document.getElementById("h3").innerHTML = "When you verify your email, click the button below"
 }
